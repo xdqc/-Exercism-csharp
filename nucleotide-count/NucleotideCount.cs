86c242ea43f8617dd7b0409fb455d3257bafdd3f
xdqc <qd6@MBP.local>
2017-08-18 22:24:45 +0800

    nucleotide

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,23 +1,36 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class DNA
 {
     public DNA(string sequence)
     {
+        this.Sequence = sequence;
     }
+
+    public string Sequence { get; set; }
 
     public IDictionary<char, int> NucleotideCounts
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return new Dictionary<char, int>(){
+                {'A', Sequence.Count(n => n=='A')},
+                {'T', Sequence.Count(n => n=='T')},
+                {'C', Sequence.Count(n => n=='C')},
+                {'G', Sequence.Count(n => n=='G')}
+            };
         }
     }
 
     public int Count(char nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!"ATCG".Contains(nucleotide))
+        {
+            throw new InvalidNucleotideException();
+        }
+        return Sequence.Count(n => n == nucleotide);
     }
 }
 
