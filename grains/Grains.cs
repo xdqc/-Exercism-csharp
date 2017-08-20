fdb910b302707ba07589eff0b20deef739173171
xdqc <qichen.d@gmail.com>
2017-08-20 23:49:22 +0800

    regex

---
+++ grains/Grains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        return (ulong)Math.Pow(2, n - 1);
+    }
+
+    public static ulong Total()
+    {
+        return (ulong)Enumerable.Range(1, 64).Sum(x => (int)Square(x));
+    }
+}
---
+++ pangram/Pangram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if(!Regex.IsMatch(input, c.ToString(), RegexOptions.IgnoreCase))
+                return false;
+        }
+        return true;
+    }
+}
