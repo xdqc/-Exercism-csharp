5ced4c83dccc4564ceb045c4d465491e25fddc78
xdqc <qichen.d@gmail.com>
2017-11-03 23:57:19 +1300

    Music scale

---
+++ scale-generator/ScaleGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ScaleGenerator
+{
+    public static string[] Pitches(string tonic, string pattern)
+    {
+        var scales = Scales(tonic);
+        var pitches = new List<string>();
+        tonic = char.ToUpper(tonic[0]) + tonic.Substring(1).ToLower();
+        int note = Array.IndexOf(scales, tonic);
+        foreach (var step in pattern.Select(c => steps[c]))
+        {
+            pitches.Add(scales[note]);
+            note = (note + step) % scales.Count();
+        }
+        return pitches.ToArray();
+    }
+
+
+    static readonly Dictionary<char, int> steps = new Dictionary<char, int>(){
+        ['m'] = 1,
+        ['M'] = 2,
+        ['A'] = 3,
+    };
+
+    static readonly string[] majorScales = new string[]{ "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+    static readonly string[] minorScales = new string[]{ "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
+    static readonly string[] flatKeys = { "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb" };
+    static string [] Scales(string tonic) => flatKeys.Contains(tonic) ? minorScales : majorScales;
+
+}
---
+++ scale-generator/ScaleGeneratorTest.cs
@@ -0,0 +1,116 @@
+﻿using Xunit;
+
+public class ScaleGeneratorTest
+{
+    [Fact]
+    public void Major_scale()
+    {
+        var major = ScaleGenerator.Pitches("C", "MMmMMMm");
+        var expected = new[] {"C", "D", "E", "F", "G", "A", "B"};
+        Assert.Equal(expected, major);
+    }
+
+    [Fact(Skip = "")]
+    public void Another_major_scale()
+    {
+        var major = ScaleGenerator.Pitches("G", "MMmMMMm");
+        var expected = new[] {"G", "A", "B", "C", "D", "E", "F#"};
+        Assert.Equal(expected, major);
+    }
+
+    [Fact(Skip = "")]
+    public void Minor_scale()
+    {
+        var minor = ScaleGenerator.Pitches("f#", "MmMMmMM");
+        var expected = new[] {"F#", "G#", "A", "B", "C#", "D", "E"};
+        Assert.Equal(expected, minor);
+    }
+
+    [Fact(Skip = "")]
+    public void Another_minor_scale()
+    {
+        var minor = ScaleGenerator.Pitches("bb", "MmMMmMM");
+        var expected = new[] {"Bb", "C", "Db", "Eb", "F", "Gb", "Ab"};
+        Assert.Equal(expected, minor);
+    }
+
+    [Fact(Skip = "")]
+    public void Dorian_mode()
+    {
+        var dorian = ScaleGenerator.Pitches("d", "MmMMMmM");
+        var expected = new[] {"D", "E", "F", "G", "A", "B", "C"};
+        Assert.Equal(expected, dorian);
+    }
+
+    [Fact(Skip = "")]
+    public void Mixolydian_mode()
+    {
+        var mixolydian = ScaleGenerator.Pitches("Eb", "MMmMMmM");
+        var expected = new[] {"Eb", "F", "G", "Ab", "Bb", "C", "Db"};
+        Assert.Equal(expected, mixolydian);
+    }
+
+    [Fact(Skip = "")]
+    public void Lydian_mode()
+    {
+        var lydian = ScaleGenerator.Pitches("a", "MMMmMMm");
+        var expected = new[] {"A", "B", "C#", "D#", "E", "F#", "G#"};
+        Assert.Equal(expected, lydian);
+    }
+
+    [Fact(Skip = "")]
+    public void Phrygian_mode()
+    {
+        var phrygian = ScaleGenerator.Pitches("e", "mMMMmMM");
+        var expected = new[] {"E", "F", "G", "A", "B", "C", "D"};
+        Assert.Equal(expected, phrygian);
+    }
+
+    [Fact(Skip = "")]
+    public void Locrian_mode()
+    {
+        var locrian = ScaleGenerator.Pitches("g", "mMMmMMM");
+        var expected = new[] {"G", "Ab", "Bb", "C", "Db", "Eb", "F"};
+        Assert.Equal(expected, locrian);
+    }
+
+    [Fact(Skip = "")]
+    public void Harmonic_minor()
+    {
+        var harmonicMinor = ScaleGenerator.Pitches("d", "MmMMmAm");
+        var expected = new[] {"D", "E", "F", "G", "A", "Bb", "Db"};
+        Assert.Equal(expected, harmonicMinor);
+    }
+
+    [Fact(Skip = "")]
+    public void Octatonic()
+    {
+        var octatonic = ScaleGenerator.Pitches("C", "MmMmMmMm");
+        var expected = new[] {"C", "D", "D#", "F", "F#", "G#", "A", "B"};
+        Assert.Equal(expected, octatonic);
+    }
+
+    [Fact(Skip = "")]
+    public void Hexatonic()
+    {
+        var hexatonic = ScaleGenerator.Pitches("Db", "MMMMMM");
+        var expected = new[] {"Db", "Eb", "F", "G", "A", "B"};
+        Assert.Equal(expected, hexatonic);
+    }
+
+    [Fact(Skip = "")]
+    public void Pentatonic()
+    {
+        var pentatonic = ScaleGenerator.Pitches("A", "MMAMA");
+        var expected = new[] {"A", "B", "C#", "E", "F#"};
+        Assert.Equal(expected, pentatonic);
+    }
+
+    [Fact(Skip = "")]
+    public void Enigmatic()
+    {
+        var enigmatic = ScaleGenerator.Pitches("G", "mAMMMmm");
+        var expected = new[] {"G", "G#", "B", "C#", "D#", "F", "F#"};
+        Assert.Equal(expected, enigmatic);
+    }
+}
