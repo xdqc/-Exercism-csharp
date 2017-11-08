8754493c11c62206994e553eafaefc246c6194a3
xdqc <qichen.d@outlook.com>
2017-11-09 03:26:10 +1300

    Double Linked List

--- linked-list/LinkedList.cs
+++ linked-list/LinkedList.cs
@@ -2,23 +2,74 @@ using System;
 
 public class Deque<T>
 {
+    private T val;
+    private Deque<T> Next, Prev, Head, Tail;
+    private bool Init;
+
+    public Deque()
+    {
+        Init = true;
+        Head = this;
+        Tail = this;
+    }
+
+    public Deque(T value)
+    {
+        this.val = value;
+        Init = false;
+        Head = this;
+        Tail = this;
+    }
+
     public void Push(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Tail.Next = new Deque<T>(value);
+        Tail.Next.Prev = Tail;
+        Tail = Tail.Next;
+        if (Init)
+        {
+            Head = Tail;
+            Init = false;
+        }
     }
 
     public T Pop()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (this != null)
+        {
+            var value = Tail.val;
+            Tail = Tail.Prev;
+            return value;
+        }
+        else
+        {
+            throw new NullReferenceException();
+        }
     }
 
     public void Unshift(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Head.Prev = new Deque<T>(value);
+        Head.Prev.Next = Head;
+        Head = Head.Prev;
+        if (Init)
+        {
+            Tail = Head;
+            Init = false;
+        }
     }
 
     public T Shift()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (this != null)
+        {
+            var value = Head.val;
+            Head = Head.Next;
+            return value;
+        }
+        else
+        {
+            throw new NullReferenceException();
+        }
     }
 }
