f72a3238fd24b60e8e1ec51f03ead39c1c35d12e
xdqc <qichen.d@outlook.com>
2017-11-12 00:56:03 +1300

    crypto square

--- crypto-square/CryptoSquare.cs
+++ crypto-square/CryptoSquare.cs
@@ -21,32 +21,27 @@ public static class CryptoSquare
 
     public static string Encoded(string plaintext)
     {
-        var textSegment = PlaintextSegments(NormalizedPlaintext(plaintext));
-        if (textSegment.Count() == 0)
-        {
+        var textSegments = PlaintextSegments(plaintext);
+        if (textSegments.Count() == 0)
             return "";
-        }
 
-        var result = "";
-        for (int i = 0; i < textSegment.First().Length; i++)
-        {
-            textSegment.ToList().ForEach(s => {
-                if (i < s.Length)
-                {
-                    result += s[i];
-                }
-            });
-        }
-        return result;
+        return string.Concat(Enumerable.Range(0, textSegments.First().Length)
+                        .Select(i => textSegments.Aggregate("", (acc, seg) => 
+                                        (i < seg.Length) ? acc+seg[i] : acc)));
     }
 
     public static string Ciphertext(string plaintext)
     {
         plaintext = Encoded(plaintext);
-        int col = (int)Math.Ceiling(Math.Sqrt(plaintext.Length));
-        for (int i = 0; i < plaintext.Length; i+=col)
-        {
-            yield return plaintext.Substring(i, Math.Min(col, (plaintext.Length-i)));
-        }
+        if (plaintext == "")
+            return "";
+
+        int row = (int)Math.Ceiling(Math.Sqrt(plaintext.Length));
+        int col = (int)Math.Floor(Math.Sqrt(plaintext.Length));
+        int fullRow = plaintext.Length / row;
+        return String.Join(" ", Enumerable.Range(0, row)
+                     .Select(i =>  (i < fullRow) 
+                        ? plaintext.Substring(i*col, col)
+                        : plaintext.Substring(fullRow*col + (i-fullRow)*(col-1), (col-1)) + ' '));
     }
 }
