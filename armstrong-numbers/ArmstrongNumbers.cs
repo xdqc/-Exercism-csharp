988c7b382f64371c2e25b46f41b3fb04564c0a17
xdqc <qichen.d@outlook.com>
2018-02-11 03:57:38 +1300

    ArmstrongNumbers

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        IEnumerable<int> digits = number.ToString().Select(d => d-'0');
+        return digits.Aggregate(0, (s, i) => s + (int)Math.Pow(i,digits.Count())) == number;
+    }
+}
