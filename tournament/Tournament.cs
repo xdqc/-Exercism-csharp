f2f25d4445b26ffa76c5d92ba023cdf1a4b8b3c7
xdqc <qichen.d@outlook.com>
2018-02-12 07:42:58 +1300

    zip array

--- tournament/Tournament.cs
+++ tournament/Tournament.cs
@@ -6,64 +6,37 @@ using System.Text;
 
 public static class Tournament
 {
+    static readonly int[] win = { 1, 1, 0, 0, 3 };
+    static readonly int[] draw = { 1, 0, 1, 0, 1 };
+    static readonly int[] loss = { 1, 0, 0, 1, 0 };
+    static void process(Dictionary<string, int[]> s, int r, string t) =>
+        s[t] = r == 0 ? s[t].Zip(draw, (x, y) => x + y).ToArray() :
+               r == 1 ? s[t].Zip(win, (x, y) => x + y).ToArray() :
+                        s[t].Zip(loss, (x, y) => x + y).ToArray();
     public static void Tally(Stream inStream, Stream outStream)
     {
         // int[5] indicates [match played ,win, draw, loss, points]
         Dictionary<string, int[]> teamStats = new Dictionary<string, int[]>();
-
         using (var reader = new StreamReader(inStream))
         {
-            // process input
             for (var record = default(string); (record = reader.ReadLine()) != null;)
             {
-
                 string[] entries = record.Split(';');
                 string team1 = entries[0];
                 string team2 = entries[1];
-                string result = entries[2];
-
-                // Add new teams to teamStats
-                if (!teamStats.ContainsKey(team1)) teamStats.Add(team1, new int[5]);
-                if (!teamStats.ContainsKey(team2)) teamStats.Add(team2, new int[5]);
-
-                switch (result)
-                {
-                    case "win":
-                        teamStats[team1][1]++;
-                        teamStats[team2][3]++;
-                        break;
-                    case "loss":
-                        teamStats[team1][3]++;
-                        teamStats[team2][1]++;
-                        break;
-                    case "draw":
-                        teamStats[team1][2]++;
-                        teamStats[team2][2]++;
-                        break;
-                    default:
-                        break;
-                }
+                int result = entries[2] == "draw" ? 0 : entries[2] == "win" ? 1 : -1;
+                if (!teamStats.ContainsKey(team1)) teamStats[team1] = new int[5];
+                if (!teamStats.ContainsKey(team2)) teamStats[team2] = new int[5];
+                process(teamStats, result, team1);
+                process(teamStats, -result, team2);
             }
         }
-
-        // caulculate points
-        teamStats.ToList().ForEach(t =>
-        {
-            t.Value[0] = t.Value[1] + t.Value[2] + t.Value[3];  //matches played
-            t.Value[4] = t.Value[1] * 3 + t.Value[2];           //points
-        });
-
-        var teams = teamStats.OrderByDescending(t => t.Value[4]).ThenBy(t => t.Key);
-
         string formatter = @"{0,-30} | {1,2} | {2,2} | {3,2} | {4,2} | {5,2}";
-
         using (var writer = new StreamWriter(outStream))
         {
             writer.WriteLine(string.Format(formatter, "Team", "MP", "W", "D", "L", "P"));
-            foreach (var team in teams)
-            {
+            foreach (var team in teamStats.OrderByDescending(t => t.Value[4]).ThenBy(t => t.Key))
                 writer.WriteLine(string.Format(formatter, team.Key, team.Value[0], team.Value[1], team.Value[2], team.Value[3], team.Value[4]));
-            }
         }
     }
 }
