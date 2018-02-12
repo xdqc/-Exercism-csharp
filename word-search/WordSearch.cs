1c7a6685722a14102a476fa6ac1133360643bede
xdqc <qichen.d@outlook.com>
2018-02-12 21:01:48 +1300

    System.Drawing point

---
+++ word-search/WordSearch.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+
+public class WordSearch
+{
+    readonly List<string> rows = new List<string>();
+    readonly List<string> cols = new List<string>();
+
+    public WordSearch(string puzzle)
+    {
+        this.rows = puzzle.Split('\n').ToList();
+        Enumerable.Range(0, rows[0].Length)
+            .Select(i => rows.Select(row => row[i]))
+            .ToList()
+            .ForEach(col => this.cols.Add(string.Concat(col)));
+    }
+
+    public Tuple<Point, Point> Find(string word) => FindWord(word) != null
+        ? FindWord(word)
+        : FindWord(string.Concat(word.Reverse())) != null
+        ? Tuple.Create(FindWord(string.Concat(word.Reverse())).Item2, FindWord(string.Concat(word.Reverse())).Item1)
+        : null;
+
+    private Tuple<Point, Point> FindWord(string word)
+    {
+        Point p1, p2 = new Point();
+        //horizontal
+        for (int i = 0; i < rows.Count; i++)
+        {
+            if (rows[i].IndexOf(word) >= 0)
+            {
+                p1.X = rows[i].IndexOf(word) + 1;
+                p2.X = rows[i].IndexOf(word) + word.Length;
+                p1.Y = i + 1;
+                p2.Y = i + 1;
+                goto FOUND;
+            }
+        }
+        //verticle
+        for (int i = 0; i < cols.Count; i++)
+        {
+            if (cols[i].IndexOf(word) >= 0)
+            {
+                p1.X = i + 1;
+                p2.X = i + 1;
+                p1.Y = cols[i].IndexOf(word) + 1;
+                p2.Y = cols[i].IndexOf(word) + word.Length;
+                goto FOUND;
+            }
+        }
+        //main diagonal
+        for (int i = 0; i < rows.Count; i++)
+        {
+            for (int j = 0; j < cols.Count; j++)
+            {
+                int k = 0, r = i, c = j;
+                while (k < word.Length && r < rows.Count && c < cols.Count && rows[r][c] == word[k])
+                    k++; r++; c++;
+                if (k == word.Length)
+                {
+                    p1.X = j + 1;
+                    p2.X = c;
+                    p1.Y = i + 1;
+                    p2.Y = r;
+                    goto FOUND;
+                }
+            }
+        }
+        //anti diagonal
+        for (int i = 0; i < rows.Count; i++)
+        {
+            for (int j = cols.Count - 1; j >= 0; j--)
+            {
+                int k = 0, r = i, c = j;
+                while (k < word.Length && r < rows.Count && c > 0 && rows[r][c] == word[k])
+                    k++; r++; c--;
+                if (k == word.Length)
+                {
+                    p1.X = j + 1;
+                    p2.X = c + 2;
+                    p1.Y = i + 1;
+                    p2.Y = r;
+                    goto FOUND;
+                }
+            }
+        }
+
+    FOUND:
+        return p1.X == 0 ? null : new Tuple<Point, Point>(p1,p2);
+    }
+}
---
+++ word-search/WordSearchTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Xunit;
+using System.Drawing;
+
+public class WordSearchTest
+{
+    private const string Puzzle =
+        "jefblpepre\n" +
+        "camdcimgtc\n" +
+        "oivokprjsm\n" +
+        "pbwasqroua\n" +
+        "rixilelhrs\n" +
+        "wolcqlirpc\n" +
+        "screeaumgr\n" +
+        "alxhpburyi\n" +
+        "jalaycalmp\n" +
+        "clojurermt";
+
+    [Fact]
+    public void Should_find_horizontal_words_written_left_to_right()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("clojure");
+        Assert.Equal(new Point(1, 10), actual.Item1);
+        Assert.Equal(new Point(7, 10), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_horizontal_words_written_right_to_left()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("elixir");
+        Assert.Equal(new Point(6, 5), actual.Item1);
+        Assert.Equal(new Point(1, 5), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_vertical_words_written_top_to_bottom()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("ecmascript");
+        Assert.Equal(new Point(10, 1), actual.Item1);
+        Assert.Equal(new Point(10, 10), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_vertical_words_written_bottom_to_top()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("rust");
+        Assert.Equal(new Point(9, 5), actual.Item1);
+        Assert.Equal(new Point(9, 2), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_diagonal_words_written_top_left_to_bottom_right()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("java");
+        Assert.Equal(new Point(1, 1), actual.Item1);
+        Assert.Equal(new Point(4, 4), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_diagonal_upper_written_bottom_right_to_top_left()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("lua");
+        Assert.Equal(new Point(8, 9), actual.Item1);
+        Assert.Equal(new Point(6, 7), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_diagonal_upper_written_bottom_left_to_top_right()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("lisp");
+        Assert.Equal(new Point(3, 6), actual.Item1);
+        Assert.Equal(new Point(6, 3), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_find_diagonal_upper_written_top_right_to_bottom_left()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("ruby");
+        Assert.Equal(new Point(8, 6), actual.Item1);
+        Assert.Equal(new Point(5, 9), actual.Item2);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_not_find_words_that_are_not_in_the_puzzle()
+    {
+        var wordSearch = new WordSearch(Puzzle);
+        var actual = wordSearch.Find("haskell");
+        Assert.Null(actual);
+    }
+
+    [Fact(Skip = "")]
+    public void Should_be_able_to_search_differently_sized_puzzles()
+    {
+        const string differentSizePuzzle =
+            "qwertyuiopz\n" +
+            "luamsicrexe\n" +
+            "abcdefghijk";
+        var wordSearch = new WordSearch(differentSizePuzzle);
+
+        var actual = wordSearch.Find("exercism");
+        Assert.Equal(new Point(11, 2), actual.Item1);
+        Assert.Equal(new Point(4, 2), actual.Item2);
+    }
+}
