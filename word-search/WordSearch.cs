3aeff054e7fbf20b8eb14faa6ac53d076e99fd8d
xdqc <qichen.d@outlook.com>
2018-02-13 04:25:26 +1300

    word matrix seach, SelectMany

--- word-search/WordSearch.cs
+++ word-search/WordSearch.cs
@@ -5,89 +5,45 @@ using System.Drawing;
 
 public class WordSearch
 {
-    readonly List<string> rows = new List<string>();
-    readonly List<string> cols = new List<string>();
+    readonly int width;
+    readonly int height;
+    readonly char[,] matrix;
 
     public WordSearch(string puzzle)
     {
-        this.rows = puzzle.Split('\n').ToList();
-        Enumerable.Range(0, rows[0].Length)
-            .Select(i => rows.Select(row => row[i]))
-            .ToList()
-            .ForEach(col => this.cols.Add(string.Concat(col)));
+        string[] rows = puzzle.Split('\n');
+        this.width = rows[0].Length;
+        this.height = rows.Length;
+        matrix = new char[width, height];
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                matrix[i, j] = rows[j][i];
     }
 
-    public Tuple<Point, Point> Find(string word) => FindWord(word) != null
-        ? FindWord(word)
-        : FindWord(string.Concat(word.Reverse())) != null
-        ? Tuple.Create(FindWord(string.Concat(word.Reverse())).Item2, FindWord(string.Concat(word.Reverse())).Item1)
-        : null;
+    private readonly static Point[] dir = new[] {
+        new Point(0, 1),
+        new Point(0, -1),
+        new Point(1, 0),
+        new Point(-1, 0),
+        new Point(1, 1),
+        new Point(-1, -1),
+        new Point(1, -1),
+        new Point(-1, 1),
+    };
 
-    private Tuple<Point, Point> FindWord(string word)
+    public Tuple<Point, Point> Find(string word)
     {
-        Point p1, p2 = new Point();
-        //horizontal
-        for (int i = 0; i < rows.Count; i++)
-        {
-            if (rows[i].IndexOf(word) >= 0)
-            {
-                p1.X = rows[i].IndexOf(word) + 1;
-                p2.X = rows[i].IndexOf(word) + word.Length;
-                p1.Y = i + 1;
-                p2.Y = i + 1;
-                goto FOUND;
-            }
-        }
-        //verticle
-        for (int i = 0; i < cols.Count; i++)
-        {
-            if (cols[i].IndexOf(word) >= 0)
-            {
-                p1.X = i + 1;
-                p2.X = i + 1;
-                p1.Y = cols[i].IndexOf(word) + 1;
-                p2.Y = cols[i].IndexOf(word) + word.Length;
-                goto FOUND;
-            }
-        }
-        //main diagonal
-        for (int i = 0; i < rows.Count; i++)
-        {
-            for (int j = 0; j < cols.Count; j++)
-            {
-                int k = 0, r = i, c = j;
-                while (k < word.Length && r < rows.Count && c < cols.Count && rows[r][c] == word[k])
-                    k++; r++; c++;
-                if (k == word.Length)
-                {
-                    p1.X = j + 1;
-                    p2.X = c;
-                    p1.Y = i + 1;
-                    p2.Y = r;
-                    goto FOUND;
-                }
-            }
-        }
-        //anti diagonal
-        for (int i = 0; i < rows.Count; i++)
-        {
-            for (int j = cols.Count - 1; j >= 0; j--)
-            {
-                int k = 0, r = i, c = j;
-                while (k < word.Length && r < rows.Count && c > 0 && rows[r][c] == word[k])
-                    k++; r++; c--;
-                if (k == word.Length)
-                {
-                    p1.X = j + 1;
-                    p2.X = c + 2;
-                    p1.Y = i + 1;
-                    p2.Y = r;
-                    goto FOUND;
-                }
-            }
-        }
-
-    FOUND:
-        return p1.X == 0 ? null : new Tuple<Point, Point>(p1,p2);
+        var result = Enumerable.Range(0, matrix.Length)
+            .SelectMany(i => dir, (n, direc) => (Idx: n, Dir: direc))
+            .FirstOrDefault(pnd => word.SequenceEqual(
+                Enumerable.Range(0, word.Length)
+                    .Select(l => new Point(pnd.Idx % width + l * pnd.Dir.Y, pnd.Idx / width + l * pnd.Dir.X))
+                    .Where(p => p.X >= 0 && p.X < width && p.Y >= 0 && p.Y < height)
+                    .Select(p => matrix[p.X, p.Y])
+            ));
+        if (result.Equals(default((int, Point)))) return null;
+        int col = result.Idx % width + 1;
+        int row = result.Idx / width + 1;
+        return Tuple.Create(new Point(col, row), new Point(col + result.Dir.Y * (word.Length - 1), row + result.Dir.X * (word.Length - 1)));
     }
 }
