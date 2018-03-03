12a686afc719e3f8d12f1bd41a74444f3fdb13ad
Samuel <qichen.d@outlook.com>
2018-03-03 18:02:57 +1300

    regex for runlengthEncoding

---
+++ nth-prime/NthPrime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class NthPrime
+{
+    public static int Prime(int nth)
+    {
+	
+	return 0;
+    }
+}
--- run-length-encoding/RunLengthEncoding.cs
+++ run-length-encoding/RunLengthEncoding.cs
@@ -1,52 +1,10 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 public static class RunLengthEncoding
 {
-    public static string Encode(string input)
-    {
-        if (input.Length <= 1)
-            return input;
-
-        int num = 1;
-        char prev = input[0];
-        string output = "";
-        input += '0';
-        for (int i = 1; i < input.Length; i++)
-        {
-            if (input[i] == prev)
-            {
-                num++;
-            }
-            else
-            {
-                output += num == 1 ? "" : num.ToString();
-                output += prev;
-                num = 1;
-            }
-            prev = input[i];
-        }
-        return output;
-    }
-
-    public static string Decode(string input)
-    {
-        if (input.Length <= 1)
-            return input;
-
-        string num = "";
-        string output = "";
-        for (int i = 0; i < input.Length; i++)
-        {
-            if (char.IsDigit(input[i]))
-            {
-                num += input[i];
-            }
-            else
-            {
-                output += num.Length == 0 ? input[i].ToString() : new string(input[i], Int32.Parse(num));
-                num = "";
-            }
-        }
-        return output;
-    }
+    public static string Encode(string input) => Regex.Replace(input, @"(\D)\1+", m => m.Length.ToString() + m.Value[0]);
+ 
+    public static string Decode(string input) => Regex.Replace(input, @"(\d+)(\D)", m => new String(m.Groups[2].Value[0], Int32.Parse(m.Groups[1].Value)));
+ 
 }
