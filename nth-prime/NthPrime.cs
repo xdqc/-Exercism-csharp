975e50333a20d72f7b121ec49afa74476c01d44f
Samuel <qichen.d@outlook.com>
2018-05-09 04:39:15 +1200

    nth prime

--- nth-prime/NthPrime.cs
+++ nth-prime/NthPrime.cs
@@ -6,7 +6,19 @@ public static class NthPrime
 {
     public static int Prime(int nth)
     {
-	
-	return 0;
+        if(nth < 1) throw new ArgumentOutOfRangeException();
+	    return Sieve().Take(nth).Last();
+    }
+
+    public static IEnumerable<int> Sieve(){
+        List<int> primes = new List<int>(){2};
+        yield return 2;
+        for (int n = 3; ; n+=2)
+        {
+            if (primes.All(p => n%p>0)){
+                primes.Add(n);
+                yield return n;
+            }
+        }
     }
 }
